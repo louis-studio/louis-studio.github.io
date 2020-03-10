aab2674e78a2a42797227870bf8eacf917edf8fd
Louis <1329415023@qq.com>
2020-03-10 13:16:21 +0800

    SYNC

--- js/script.js
+++
@@ -1,6 +0,0 @@
-window.onfocus = function() {
-    document.title = '创新改变世界!';
-};
-window.onblur = function() {
-    document.title = 'LOUIS STUDIO';
-};
---
+++ js/type.js
@@ -0,0 +1,7 @@
+var typed = new Typed("#typed", {
+    strings: ["我们用心做产品 ^500 因为用心才能做好 ^500", "我们创新 ^500 因为创新改变世界 ^500", "用心创造未来"],
+    showCursor: false,
+    startDelay: 200,
+    typeSpeed: 50,
+    backSpeed: 25
+});
