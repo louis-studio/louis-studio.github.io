2f3f32ae0071c11038e7d322f7125f72534f5c7a
Louis <1329415023@qq.com>
2020-03-11 16:13:07 +0800

    f

--- js/type.js
+++ js/type.js
@@ -1,7 +1,9 @@
 var typed = new Typed("#typed", {
-    strings: ["我们用心做产品 ^500 因为用心才能做好 ^500", "我们创新 ^500 因为创新改变世界 ^500", "用心创造未来"],
+    strings: ["我们用心做产品 ^500 因为用心才能做好 ^500", "我们创新 ^500 因为创新改变世界 ^500", "用心创造未来 ^500", "用户至上 ^500", "Less is more ^500"],
     showCursor: false,
     startDelay: 200,
     typeSpeed: 50,
-    backSpeed: 25
+    backSpeed: 25,
+    loop: true,
+    loopCount: Infinity
 });
